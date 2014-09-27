ebdf52c678c3e0390c00d02785b557f84f7794ac
miracle2 <tami.ybp@gmail.com>
2014-09-27 12:11:11 -0400

    more edits and stuff

--- code.js
+++ code.js
@@ -5,14 +5,22 @@ day = test.getDate();
 year = test.getFullYear();
 document.write("<p> ",month,"/",day,"/",year," </p>");
 
+var log=
+
 function myFunction() {
 	var demoElement = document.getElementById("demo");
 	var hist=document.getElementById("history");
-	hist.innerHTML = "<p>earioghoer;h</p>" + hist.innerHTML;
+	var newel = document.createElement("div");
+	newel.innerText = "earioghoer;h";
+	hist.appendChild(newel);
+	// hist.removeChild(newel);
+	//hist.innerHTML = "<p>earioghoer;h</p>" + hist.innerHTML;
     if(demoElement.style.color=="red"){
     	demoElement.style.color = "green";
     }
     else demoElement.style.color = "red";
+}
 
-    
+function clearHidden(){
+	
 }
