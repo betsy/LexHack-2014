7a770a302b5c0672be5af2b71482f7e1fbee3519
miracle2 <tami.ybp@gmail.com>
2014-09-27 13:19:49 -0400

    added text

--- code.js
+++ code.js
@@ -5,11 +5,15 @@ day = test.getDate();
 year = test.getFullYear();
 document.write("<p> ",month,"/",day,"/",year," </p>");
 
+var narration = ["A long, long time ago, in a galaxy far, far away, you are a prisoner on a maximum security automated galactic prison ship and you seem to have forgotten everything about your past and who you are.","For the first time in years, a solar flare causes a power outage on the ship and the locks are disabled...what do you do?"];
+var stage=0;
+
 function myFunction() {
 	var demoElement = document.getElementById("demo");
 	var hist=document.getElementById("history");
 	var newel = document.createElement("div");
-	newel.innerText = "earioghoer;h";
+	newel.innerText = narration[stage%narration.length];
+	stage++;
 	hist.appendChild(newel);
 	// hist.removeChild(newel);
 	//hist.innerHTML = "<p>earioghoer;h</p>" + hist.innerHTML;
